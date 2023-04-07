7629ea3522d30aa09247e83937c61c8381d105af
kissurda93 <jg19930610@gmail.com>
2023-04-07 16:02:24 +0200

    CurrencyConverter switched to amrshawky/laravel-currency

--- app/Services/CurrencyConverter.php
+++ app/Services/CurrencyConverter.php
@@ -2,54 +2,26 @@
 
 namespace App\Services;
 
-use Illuminate\Http\Client\Pool;
 use Illuminate\Support\Collection;
-use Illuminate\Support\Facades\Http;
+use AmrShawky\LaravelCurrency\Facade\Currency;
 
 class CurrencyConverter
 {
-  private $apiKey;
-  private $url;
-
-  public function __construct()
-  {
-    $this->url = 'https://api.apilayer.com/exchangerates_data/convert';
-    $this->apiKey = config('simplePay.CONVERTER_API_KEY');
-  }
-
   public function convert(Collection $products, int $totalPrice): array
   {
-
-    $responses = Http::pool(function (Pool $pool) use ($products, $totalPrice) {
-      $products->each(function($product) use ($pool) {
-        $pool
-        ->withHeaders([
-          'apikey' => $this->apiKey,
-        ])
-        ->get($this->url, [
-        'from' => 'USD',
-        'to' => 'HUF',
-        'amount' => $product['price'],
-      ]);
-      });
-
-      $pool
-      ->as('total')
-      ->withHeaders([
-        'apikey' => $this->apiKey,
-      ])
-      ->get($this->url, [
-        'from' => 'USD',
-        'to' => 'HUF',
-        'amount' => $totalPrice,
-      ]);
+    $products->each(function($product) {
+      $product['price'] = Currency::convert()
+      ->from('USD')
+      ->to('HUF')
+      ->amount($product['price'])
+      ->get();
     });
 
-    for ($i = 0; $i <= count($products) - 1; $i++) {
-        $products[$i]['price'] = round($responses[$i]['result']);
-    }
-
-    $totalPrice = round($responses['total']['result']);
+    $totalPrice = Currency::convert()
+    ->from('USD')
+    ->to('HUF')
+    ->amount($totalPrice)
+    ->get();
 
     return [$products, $totalPrice];
   }
